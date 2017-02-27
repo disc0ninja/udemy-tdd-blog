f55b351baaefc84617b490a70f66badec1f65c11
disc0ninja <disc0ninja77@gmail.com>
2017-02-26 21:01:34 -0600

    Testing: Feature: Articles: Added test coverage for article creation failure if the user specifically forgets just the title, but has a body, or vice versa

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -16,7 +16,7 @@ RSpec.feature "Creating Articles" do
     # expect success message
     expect(page).to have_content("Article has been created successfully")
     # Redirects to the articles path (where the article is listed)
-    current_path.should == "/articles"
+    expect(current_path).to eq("/articles")
   end
 
   scenario "A user fails to create a new article without a title or body" do
@@ -36,4 +36,41 @@ RSpec.feature "Creating Articles" do
     expect(page).to have_content("Title can't be blank")
     expect(page).to have_content("Body can't be blank")
   end
+
+  scenario "A user fails to create a new article without a title" do
+    # Visit root
+    visit "/"
+    # Click new article link
+    first(:link, "New Article").click
+    # Fill in title of article
+    fill_in "Title", with: ""
+    # Fill in the body of the article
+    fill_in "Body", with: "asdf asdf asdf asdf asdf asdf asdf"
+    # Create article
+    click_button "Create Article"
+
+    # expect faiure messages
+    expect(page).to have_content("Article has not been created")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_no_content("Body can't be blank")
+  end
+
+  scenario "A user fails to create a new article without a body" do
+    # Visit root
+    visit "/"
+    # Click new article link
+    first(:link, "New Article").click
+    # Fill in title of article
+    fill_in "Title", with: "asdf"
+    # Fill in the body of the article
+    fill_in "Body", with: ""
+    # Create article
+    click_button "Create Article"
+
+    # expect faiure messages
+    expect(page).to have_content("Article has not been created")
+    expect(page).to have_no_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+  end
+
 end
