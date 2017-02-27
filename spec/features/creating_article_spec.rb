29ea73ebf59201f650eea1a3ae491d878f1589cb
disc0ninja <disc0ninja77@gmail.com>
2017-02-26 20:51:34 -0600

    Testing: Added scenario for creating an article failing Feature: Article: Validations: Added validations to articles being created that check for the presence of title and body, and added an error display upon unsuccesful article creation that will display the count of errors, and the error messages that are preventing the articles creation

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -16,6 +16,24 @@ RSpec.feature "Creating Articles" do
     # expect success message
     expect(page).to have_content("Article has been created successfully")
     # Redirects to the articles path (where the article is listed)
-    expect(page).to eq(articles_path)
+    current_path.should == "/articles"
+  end
+
+  scenario "A user fails to create a new article without a title or body" do
+    # Visit root
+    visit "/"
+    # Click new article link
+    first(:link, "New Article").click
+    # Fill in title of article
+    fill_in "Title", with: ""
+    # Fill in the body of the article
+    fill_in "Body", with: ""
+    # Create article
+    click_button "Create Article"
+
+    # expect faiure messages
+    expect(page).to have_content("Article has not been created")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
   end
 end
