bf2babddfeae4e8668441f40a8bae612483b55a2
disc0ninja <disc0ninja77@gmail.com>
2017-02-28 00:36:25 -0600

    Testing: Feature: Articles: Show: Added test for article show, that creates an article and has the user navigate to the created articles show page, where it checks for content of the created article
    Feature: Article: Show:
    
    Added article show action in articles controller
    Added article show template that is used to display articles in an mdl card
    when a user navigates to the articles show page. Articles display in their entirety
    
    Styling: Major updates to styling!
    Deleted unecessary and unused sass files
    renamed all sass files to eliminate the .css which is no longer required when using sass
    added layout styling and several mixins one for cross browser calc, and one for referencing all heading sizes
    
    Styled article show page with mdl, and everything is styled to be responsive

---
+++ spec/features/show_article_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.feature "Show Article" do
+# Create an article for user to show
+  before do
+    @article = Article.create(title: "Article Title", body: "This is some article body content")
+  end
+
+  scenario "A user shows an article" do
+    # visit root
+    visit "/"
+    # click the title link
+    click_link @article.title
+    # expect the page to have article title
+    expect(page).to have_content(@article.title)
+    # expect the page to have article body
+    expect(page).to have_content(@article.body)
+    # expect the current_path to be the same as the article that's created
+    expect(current_path).to eq(article_path(@article))
+  end
+
+end
