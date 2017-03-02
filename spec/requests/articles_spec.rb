b6fd294dcbce4124fefab973b2a85eb3f2586e3a
disc0ninja <disc0ninja77@gmail.com>
2017-03-02 16:22:11 -0600

    Testing: Requests: Articles: Delete: Updated requests spec to include test coverage on the DELETE method for articles. Test ensures a non signed in user receives a redircect and error message asking them to sign up or sign in to continue. Also tests that a signed in non owner trying to perform a delete action is redirecteda and recieves an error message explaining they can only delete their own article. Finally the test checks for a signed in and article owner that tries to delete an article, gets a redirection and a success message
    Updated articles_controller destroy method to make sure only signed in article owners are able to delete their respective articles.

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -65,4 +65,43 @@ RSpec.describe "Articles", type: :request do
     end
   end
 
+  describe "DELETE /articles/:id" do
+    context "with non signed in user" do
+      before { delete "/articles/#{@article.id}" }
+
+      it "redirects to the signin page" do
+        expect(response.status).to eq 302
+        flash_message = "You need to sign in or sign up before continuing."
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+
+    context "with signed in user that is non-owner" do
+      before do
+        login_as(@john)
+        delete "/articles/#{@article.id}"
+      end
+
+      it "redirects to the home page" do
+        expect(response.status).to eq 302
+        flash_message = "You can only delete your own articles"
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+
+    context "with owner signed in successfull delete" do
+      before do
+        login_as(@fred)
+        delete "/articles/#{@article.id}"
+      end
+
+      it "successfully deletes article" do
+        expect(response.status).to eq 302
+        flash_message = "Article has been deleted"
+        expect(flash[:success]).to eq flash_message
+      end
+
+    end
+  end
+
 end
